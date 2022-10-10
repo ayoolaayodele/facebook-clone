271734736fb9afd09bac12108959aab6f3bace4b
Ayodele <ayodeleayoola264@gmail.com>
2022-10-10 18:03:43 +0100

    Implemented activate account and user login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const {
   validateEmail,
   validateLength,
@@ -72,11 +73,77 @@ exports.register = async (req, res) => {
     }).save();
     const emailVerificationToken = generateToken({ id: user._id }, "30m");
 
-    user.password = undefined;
-    console.log(emailVerificationToken);
     const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
     sendVerificationEmail(user.email, user.first_name, url);
-    res.status(201).json(user);
+    const token = generateToken({ id: user._id }, "7d");
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: "Register Success ! please activate your email to start",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.activateAccount = async (req, res) => {
+  try {
+    const validUser = req.user.id;
+    const { token } = req.body;
+    const user = jwt.verify(token, process.env.TOKEN_SECRET);
+    const check = await User.findById(user.id);
+
+    if (validUser !== user.id) {
+      return res.status(400).json({
+        message: "You don't have the authorization to complete this operation.",
+      });
+    }
+    if (check.verified == true) {
+      return res
+        .status(400)
+        .json({ message: "This email is already activated." });
+    } else {
+      await User.findByIdAndUpdate(user.id, { verified: true });
+      return res
+        .status(200)
+        .json({ message: "Account has beeen activated successfully." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message:
+          "the email address you entered is not connected to an account.",
+      });
+    }
+    const check = await bcrypt.compare(password, user.password);
+    if (!check) {
+      return res.status(400).json({
+        message: "Invalid credentials.Please try again.",
+      });
+    }
+    const token = generateToken({ id: user._id }, "7d");
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
