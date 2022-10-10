7f7b12c43315a1a53ea432bd8e7ded9eae9d4c7f
Ayodele <ayodeleayoola264@gmail.com>
2022-10-10 19:08:21 +0100

    Added activate and login route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const userController = require("../controllers/userController");
+const { authUser } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.post("/register", userController.register);
+router.post("/activate", authUser, userController.activateAccount);
+router.post("/login", userController.login);
 
 module.exports = router;
