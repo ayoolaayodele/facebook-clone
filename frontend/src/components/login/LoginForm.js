114277a19e6f6a4345d00ed0a2de52becce013c7
Ayodele <ayodeleayoola264@gmail.com>
2022-10-12 13:04:51 +0100

    Created login Input components, footer, login form, and register form component and wrote some conditional statements to render formik errors, Created

---
+++ frontend/src/components/login/LoginForm.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { Link } from "react-router-dom";
+import LoginInput from "../../components/inputs/loginInput";
+
+const loginInfo = {
+  email: "",
+  password: "",
+};
+
+function LoginForm() {
+  const [login, setLogin] = useState(loginInfo);
+  const { email, password } = login;
+
+  const handleLoginChange = function (e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setLogin({ ...login, [name]: value });
+  };
+  const loginValidation = Yup.object({
+    email: Yup.string()
+      .required("Email address is required.")
+      .email("Must be a valid email.")
+      .max(100),
+    password: Yup.string().required("Password is required"),
+  });
+  return (
+    <div className="login_wrap">
+      <div className="login_1">
+        <img src="../../icons/facebook.svg" alt="" />
+        <span>
+          Facebook helps you connect and share with the people in your life.
+        </span>
+      </div>
+      <div className="login_2">
+        <div className="login_2_wrap">
+          {" "}
+          <Formik
+            enableReinitialize
+            initialValues={{
+              email,
+              password,
+            }}
+            validationSchema={loginValidation}
+          >
+            {(formik) => (
+              <Form>
+                <LoginInput
+                  type="text"
+                  name="email"
+                  placeholder="Email Address or phone number"
+                  onChange={handleLoginChange}
+                />
+                <LoginInput
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  onChange={handleLoginChange}
+                  bottom
+                />
+                <button type="submit" className="blue_btn">
+                  Log In
+                </button>
+              </Form>
+            )}
+          </Formik>
+          <Link to="/reset" className="forgot_password">
+            Forgotten Password?
+          </Link>
+          <div className="sign_splitter"></div>
+          <button className="blue_btn open_signup">Create New Account</button>
+        </div>
+        <Link to="/" className="sign_extra">
+          <b>Create a Page</b> for a celebrity, brand or business
+        </Link>{" "}
+      </div>
+    </div>
+  );
+}
+
+export default LoginForm;
