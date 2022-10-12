b93d0c1d7f9617c3551453a1b2fb4b7a16d8c35e
Ayodele <ayodeleayoola264@gmail.com>
2022-10-12 13:20:36 +0100

    Set up reducers

---
+++ frontend/src/reducers/userReducer.js
@@ -0,0 +1,10 @@
+import { LOGIN } from "../actions";
+
+export function userReducer(state = null, action) {
+  switch (action.type) {
+    case LOGIN:
+      return action.payload;
+    default:
+      return state;
+  }
+}
