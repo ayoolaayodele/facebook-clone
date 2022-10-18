b1d77cc679135a38f117430c57beae67b1c89c80
Ayodele <ayodeleayoola264@gmail.com>
2022-10-18 17:47:15 +0100

    Created home components

---
+++ frontend/src/components/home/left/index.js
@@ -0,0 +1,83 @@
+import LeftLink from "./LeftLink";
+import "./style.css";
+import { left } from "../../../data/home";
+import { Link } from "react-router-dom";
+import { ArrowDown1 } from "../../../svg";
+import { useState } from "react";
+import Shortcut from "./Shortcut";
+function LeftHome({ user }) {
+  const [visible, setShowVisible] = useState(false);
+  return (
+    <div className="left_home scrollbar">
+      <Link to="/profile" className="left_link hover1">
+        <img src={user?.picture} alt="" />
+        <span>
+          {user?.first_name} {user.last_name}
+        </span>
+      </Link>
+      {left.slice(0, 8).map((item, i) => {
+        return <LeftLink {...item} key={i} />;
+      })}
+      {!visible && (
+        <div className="left_link hover1" onClick={() => setShowVisible(true)}>
+          <div className="small_circle">
+            <ArrowDown1 />
+          </div>
+          <span>See more</span>
+        </div>
+      )}
+      {visible && (
+        <div className="more_left">
+          {left.slice(8, left.length).map((item, i) => {
+            return <LeftLink {...item} key={i} />;
+          })}
+          <div
+            className="left_link hover1"
+            onClick={() => setShowVisible(false)}
+          >
+            <div className="small_circle rotate360">
+              <ArrowDown1 />
+            </div>
+            <span>Show less</span>
+          </div>
+        </div>
+      )}
+      <div className="splitter"></div>
+      <div className="shortcut">
+        <div className="heading">Your Shortcuts</div>
+        <div className="edit_shortcut">Edit</div>
+      </div>
+      <div className="shortcut_list">
+        <Shortcut
+          link="https://www.youtube.com/c/MohamedHaJJi1/featured"
+          img="../../images/ytb.png"
+          name="My Youtube channel"
+        />
+
+        <Shortcut
+          link="https://www.instagram.com/med_hajji7/"
+          img="../../images/insta.png"
+          name="My Instagram "
+        />
+      </div>
+      <div className={`fb_copyright ${visible && "relative_fb_copyright"}`}>
+        <Link to="/">Privacy </Link>
+        <span>. </span>
+        <Link to="/">Terms </Link>
+        <span>. </span>
+        <Link to="/">Advertising </Link>
+        <span>. </span>
+        <Link to="/">
+          Ad Choices <i className="ad_choices_icon"></i>{" "}
+        </Link>
+        <span>. </span>
+        <Link to="/"></Link>Cookies <span>. </span>
+        <Link to="/">More </Link>
+        <span>. </span> <br />
+        Meta © 2022
+      </div>
+    </div>
+  );
+}
+
+export default LeftHome;
