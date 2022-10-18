96cc96f86ff39dba20086d49dc094b119698a78d
Ayodele <ayodeleayoola264@gmail.com>
2022-10-18 17:43:59 +0100

    Passed user data to home components

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,7 +1,18 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import Header from "../../components/header";
+import Left from "../../components/home/left";
+import RightHome from "../../components/home/right";
 
 function Home() {
-  return <div>Home</div>;
+  const { user } = useSelector((user) => ({ ...user }));
+
+  return (
+    <div>
+      <Header />
+      <Left user={user} />
+      <RightHome user={user} />
+    </div>
+  );
 }
 
 export default Home;
