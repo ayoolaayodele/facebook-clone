f61794f9a8635171ceb739e666bac2410c667d43
Ayodele <ayodeleayoola264@gmail.com>
2022-10-23 19:23:45 +0100

    Implemeted an action to handle logout

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import { LOGIN } from "../actions";
+import { LOGIN, LOGOUT, VERIFY } from "../actions";
 import Cookies from "js-cookie";
 
 export function userReducer(
@@ -8,6 +8,10 @@ export function userReducer(
   switch (action.type) {
     case LOGIN:
       return action.payload;
+    case LOGOUT:
+      return null;
+    case VERIFY:
+      return { ...state, verified: action.payload };
     default:
       return state;
   }
