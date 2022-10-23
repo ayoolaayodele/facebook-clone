ca5a9756b90b3fe4ef8ac06dc4397156d86ef8e2
Ayodele <ayodeleayoola264@gmail.com>
2022-10-23 19:31:35 +0100

    Implemented a conditional statement to conditionally render create post pop up and also created routes to verify token sent from server, created a routes to reset the password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,32 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
+import CreatePostPopup from "./components/createPostPopup";
 import Home from "./pages/home";
+import Activate from "./pages/home/activate";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
+import Reset from "./pages/reset";
 import LoggedInRoutes from "./routes/LoggedInRoutes";
 import NotLoggedInRoutes from "./routes/NotLoggedInRoutes";
 
 function App() {
+  const [visible, setVisible] = useState(false);
+  const { user } = useSelector((user) => ({ ...user }));
+
   return (
     <div>
+      {visible && <CreatePostPopup user={user} setVisible={setVisible} />}
       <Routes>
         <Route element={<LoggedInRoutes />}>
           <Route path="/profile" element={<Profile />} exact />
-          <Route path="/" element={<Home />} exact />
+          <Route path="/" element={<Home setVisible={setVisible} />} exact />
+          <Route path="/activate/:token" element={<Activate />} exact />
         </Route>
         <Route element={<NotLoggedInRoutes />}>
           <Route path="/login" element={<Login />} exact />
         </Route>
+        <Route path="/reset" element={<Reset />} />
       </Routes>
     </div>
   );
