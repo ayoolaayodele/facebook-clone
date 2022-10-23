5a8c61951d4d887cb2d6ed9d841b01d65aa8c20b
Ayodele <ayodeleayoola264@gmail.com>
2022-10-23 19:46:33 +0100

    Created routes for send-verification, to find user, for code resend, to validate code, to change password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,5 +7,10 @@ const router = express.Router();
 router.post("/register", userController.register);
 router.post("/activate", authUser, userController.activateAccount);
 router.post("/login", userController.login);
+router.post("/sendVerification", authUser, userController.sendVerification);
+router.post("/findUser", userController.findUser);
+router.post("/sendResetPasswordCode", userController.sendResetPasswordCode);
+router.post("/validateResetCode", userController.validateResetCode);
+router.post("/changePassword", userController.changePassword);
 
 module.exports = router;
