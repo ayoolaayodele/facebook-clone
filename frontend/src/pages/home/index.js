cdc79e16b5a5c6536434e2f837bf1544fffa3a8d
Ayodele <ayodeleayoola264@gmail.com>
2022-10-23 19:26:32 +0100

    Used AND operator to conditionally render send verifcation components

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import CreatePost from "../../components/createPost";
 import Header from "../../components/header";
 import Left from "../../components/home/left";
 import RightHome from "../../components/home/right";
+import SendVerification from "../../components/home/sendVerification";
 import Stories from "../../components/home/stories";
 import "./style.css";
 
-function Home() {
+function Home({ setVisible }) {
   const { user } = useSelector((user) => ({ ...user }));
 
   return (
@@ -15,7 +17,8 @@ function Home() {
       <Left user={user} />
       <div className="home_middle">
         <Stories />
-        <CreatePost user={user} />
+        {user.verified === false && <SendVerification user={user} />}
+        <CreatePost user={user} setVisible={setVisible} />
       </div>
       <RightHome user={user} />
     </div>
