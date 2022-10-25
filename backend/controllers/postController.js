8a54e2222e630aab621d4df7706e1531ad0723c5
Ayodele <ayodeleayoola264@gmail.com>
2022-10-25 07:51:36 +0100

    Implemented getAllPosts functions to fetch all posts and populated with needed data

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -5,7 +5,20 @@ exports.createPost = async (req, res) => {
     res.status(201).json(post);
   } catch (error) {
     res.status(500).json({
-      err: error.message,
+      message: error.message,
+    });
+  }
+};
+
+exports.getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .populate("user", "first_name last_name picture username gender")
+      .sort({ createdAt: -1 });
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
     });
   }
 };
