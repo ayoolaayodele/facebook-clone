31b573d9d63760952566753bbfe5f14e42c02e25
Ayodele <ayodeleayoola264@gmail.com>
2022-10-25 07:46:18 +0100

    Used useRef hook to access the clientHeight to set the home background color to full height

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import CreatePost from "../../components/createPost";
 import Header from "../../components/header";
@@ -6,19 +6,30 @@ import Left from "../../components/home/left";
 import RightHome from "../../components/home/right";
 import SendVerification from "../../components/home/sendVerification";
 import Stories from "../../components/home/stories";
+import Post from "../../components/posts";
 import "./style.css";
 
-function Home({ setVisible }) {
+function Home({ setVisible, posts }) {
   const { user } = useSelector((user) => ({ ...user }));
+  const middle = useRef(null);
+  const [height, setHeight] = useState();
+  useEffect(() => {
+    setHeight(middle.current.clientHeight);
+  }, []);
 
   return (
-    <div className="home">
+    <div className="home" style={{ height: `${height + 150}px` }}>
       <Header />
       <Left user={user} />
-      <div className="home_middle">
+      <div className="home_middle" ref={middle}>
         <Stories />
         {user.verified === false && <SendVerification user={user} />}
         <CreatePost user={user} setVisible={setVisible} />
+        <div className="posts">
+          {posts.map((post) => {
+            return <Post key={post._id} post={post} user={user} />;
+          })}
+        </div>
       </div>
       <RightHome user={user} />
     </div>
